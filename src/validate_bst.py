8e61ea19b3948da5b709cf3cdadea0910172d69f
Mohammad Tanweer <tanweer@MBP5C02T2H5LVDM.broadband>
2022-04-25 15:48:57 +0100

    Validate Binary Search Tree

---
+++ src/validate_bst.py
@@ -0,0 +1,58 @@
+from typing import Optional, List
+import math
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f"val: {self.val}, left: {self.left}, right: {self.right}"
+
+    def __str__(self) -> str:
+        return str(self.val)
+
+
+class Validate_BST:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def validate(node, low=-math.inf, high=math.inf):
+            # Empty trees are valid BSTs.
+            if not node:
+                return True
+            # The current node's value must be between low and high.
+            if node.val <= low or node.val >= high:
+                return False
+
+            # The left and right subtree must also be valid.
+            return (validate(node.right, node.val, high) and
+                   validate(node.left, low, node.val))
+
+        return validate(root)
+
+
+def to_binary_tree(items: List[int]) -> TreeNode:
+    """Create BT from list of values."""
+    n = len(items)
+    if n == 0:
+        return None
+
+    def inner(index: int = 0) -> TreeNode:
+        """Closure function using recursion bo build tree"""
+        if n <= index or items[index] is None:
+            return None
+
+        node = TreeNode(items[index])
+        node.left = inner(2 * index + 1)
+        node.right = inner(2 * index + 2)
+        return node
+    return inner()
+
+
+if __name__ == "__main__":
+    obj = Validate_BST()
+    inp = [10, 5, 15, 3, 7, None, 17]
+    root = to_binary_tree(inp)
+    print(obj.isValidBST(root))
