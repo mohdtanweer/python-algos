98dd6f933ff9df83afe81009923e9b3f8faaaa52
Mohammad Tanweer <tanweer@MBP5C02T2H5LVDM.broadband>
2022-04-26 09:14:16 +0100

    Add two numbers

--- src/AddTwoNumbers.py
+++ src/AddTwoNumbers.py
@@ -25,15 +25,23 @@ class AddTwoNumbers:
             ret.next = self.addTwoNumbers(l1.next, l2.next, c)
         return ret
 
+def creat_linked_list(arr):
+    head = None
+    for a in arr:
+        head = ListNode(a, next=head)
+    return head
+
+
 if __name__ == "__main__":
+    list1 = [9, 9, 9, 9, 9, 9, 9]
+    list2 = [9, 9, 9, 9]
+    llist1 = creat_linked_list(list1)
+    llist2 = creat_linked_list(list2)
     obj = AddTwoNumbers()
-    l1 = ListNode(0)
-    l2 = ListNode(0)
-    print(l1, type(l1))
-    list1 = [2, 4, 3]
-    list2 = [5, 6, 4]
-    for l in list1:
-        l1.val = l
-        l1.next = ListNode(0)
-    print(l1.val, l1.next)
-    # print(obj.addTwoNumbers(l1, l2))
+    # print(obj.addTwoNumbers(llist1, llist2))
+    x = obj.addTwoNumbers(llist1, llist2)
+    arr = []
+    while x != None:
+        arr.append(x.val)
+        x = x.next
+    print(arr)
